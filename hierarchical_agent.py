83b737192456a41278562b26c2b24e644897b2e8
Alex Rose <rosea@tf.uni-freiburg.de>
2019-01-23 15:03:52 +0100

    important improvements

--- hierarchical_agent.py
+++ hierarchical_agent.py
@@ -1,4 +1,13 @@
 from agent import BaseAgent
 
 class HierarchicalAgent(BaseAgent):
+    def __init__(self, state_space, action_space):
+        # self.state_space = state_space
+        # self.action_space = action_space
+        super().__init__(state_space, action_space)
     
+    def act(self, state):
+        raise NotImplementedError
+
+    def train(self, state, action, reward: float, next_state, done: bool):
+        raise NotImplementedError
