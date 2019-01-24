69f868a4bd0a9b49abe23a85ddb3d46570b50792
Alex Rose <rosea@tf.uni-freiburg.de>
2019-01-24 16:13:35 +0100

    hierarchical stuff

--- agent.py
+++ agent.py
@@ -24,48 +24,151 @@ class BaseAgent:
               relabel=False):
         raise NotImplementedError
 
+    @staticmethod
+    def relabel_goal(original_goal,
+                     lo_state_seq=None,
+                     lo_action_seq=None,
+                     lo_current_policy=None):
+        """
+        a dummy copy of the HiAgent's goal relabeler, which agents can call with no effect
+        when running as low level
+        """
+        return original_goal
+
 
 class HiAgent(BaseAgent):
+    """
+    Agents capable of functioning as High-Level agents in the HIRO algorithm should inherit this class
+    """
+
     def train(self,
               state,
               action,
               reward: float,
               next_state,
               done: bool,
-              relabel=True,
+              relabel=False,
               lo_state_seq=None,
               lo_action_seq=None,
               lo_current_policy=None):
-        # lo_xx_seq will be a c-tuple of the states seen / actions taken by lo_agent to achieve the goal
-        # needed for relabelling transition tuples later
+        """
+        a version of train() with extra arguments required by high-level agents
+        for relabelling transition tuples later
 
-        # at some point, we should have
-        #   if relabel: transition_tuple = relabel_goal(...)
-        #   else: transition_tuple = ...
+        Parameters
+        ----------
+
+        lo_state_seq : np.array: (c, *state.shape)
+            array containing the c states visited by the LoAgent since the last HiAgent training step
+        
+        lo_action_seq : np.array: (c, *action.shape)
+            array containing the c actions taken by the LoAgent since the last HiAgent training step
+
+        lo_current_policy : func: state -> action
+            the act() function of the LoAgent (supplied by a MetaAgent)
+        """
 
         raise NotImplementedError
 
-    def relabel_goal(self, original_goal, lo_state_seq, lo_action_seq,
+    @staticmethod
+    def relabel_goal(original_goal, lo_state_seq, lo_action_seq,
                      lo_current_policy):
-        # this will be used internally by the HiAgent in its train() routine
+        """
+        this will be used internally by the HiAgent in its train() routine
+        where, at some point, we should have
+            if relabel: 
+                transition_tuple = relabel_goal(...)
+            else: 
+                transition_tuple = ...
+
+        Parameters
+        ----------
+
+        original_goal : np.array: (*state.shape)
+
+
+        lo_state_seq : np.array: (c, *state.shape)
+            array containing the c states visited by the LoAgent since the last HiAgent training step
+        
+        lo_action_seq : np.array: (c, *action.shape)
+            array containing the c actions taken by the LoAgent since the last HiAgent training step
+
+        lo_current_policy : func: state -> action
+            the act() function of the LoAgent (supplied by a MetaAgent)        
+        """
 
         # get variance from observed states
-        stdev_goal = np.std(lo_state_seq)
+        stdev_goal = np.std(lo_state_seq, axis=0)  #todo - cov instead
 
         # eight candidate goals sampled randomly from a Gaussian centered at s_t+c − s_t
         # (since goals implemented as states, this just becomes s_t+c)
+        n_candidate_goals = 8
 
         candidate_goals = np.random.normal(
-            loc=original_goal, scale=stdev_goal, size=8
+            loc=original_goal,
+            scale=stdev_goal,
+            size=(n_candidate_goals, *original_goal.shape))
 
-        # also include the original goal gt and a goal corresponding to the difference st+c − st
+        # also include the original goal gt 
+        # and a goal corresponding to the difference st+c − st
         candidate_goals = np.concatenate([
             candidate_goals,
             np.expand_dims(original_goal, axis=0),
             np.expand_dims(lo_state_seq[-1] - lo_state_seq[0], axis=0)
-            ], axis=0)
+        ],
+                                         axis=0)
 
-        # find the (approximate) argmax goal to relabel with
-        lo_policy_likelihood = lo_current_policy()
+        lo_policy_likelihoods = []
 
-        raise NotImplementedError
+        # running this as a for loop is not quite optimal -- todo later
+        for g in range(n_candidate_goals + 2):
+
+            # transform the (state) c-tuple into a (state, goal) c-tuple
+            # shape = (c, 2, *state_shape)
+            lo_stategoal_seq = np.stack([
+                lo_state_seq,
+                np.broadcast_to(candidate_goals[g], lo_state_seq.shape)
+            ],
+                                        axis=1)
+
+            # what actions would the current LoAgent take, given goal g?
+            lo_current_actions = lo_current_policy(lo_stategoal_seq)
+
+            # how far do they diverge from the actual actions, given original goal?
+            # shape = (c, *action_shape)
+            lo_sq_difference = np.linalg.norm(lo_action_seq -
+                                              lo_current_actions)**2
+
+            lo_neg_sum_sq_diff = -1 * np.sum(lo_sq_difference, axis=0)
+
+            lo_policy_likelihoods.append(lo_neg_sum_sq_diff)
+
+        # find the (approximate) goal that maximises the likelihood of the observed actions
+        likeliest_goal = np.argmax(lo_policy_likelihoods)
+
+        return candidate_goals[likeliest_goal]
+
+class Pigeon(HiAgent):
+    """
+    a stupid agent for testing purposes
+    """
+    def act(self, state, training=False):
+
+        return np.random.uniform(self.action_space.low, self.action_space.high)
+
+    def train(self,
+              state,
+              action,
+              reward: float,
+              next_state,
+              done: bool,
+              relabel=False,
+              lo_state_seq=None,
+              lo_action_seq=None,
+              lo_current_policy=None):
+
+        if relabel:
+            # just to make sure this method functions as it should
+            relabeled_goal = self.relabel_goal(action, lo_state_seq, lo_action_seq, lo_current_policy)
+
+        pass
--- meta_agent.py
+++ meta_agent.py
@@ -2,15 +2,23 @@ from agent import BaseAgent, HiAgent
 import gym
 import numpy as np
 
+import agent
+from importlib import reload
+reload(agent)
+
 
 class MetaAgent(BaseAgent):
+    """
+    a meta-agent for "Data Efficient Hierarchical Learning (HIRO)"
+    """
+
     def __init__(self,
                  state_space,
                  action_space,
                  hi_agent=HiAgent,
                  lo_agent=BaseAgent,
                  c=10):
-        # note, this will not work if initialised with 
+        # note, this will not work if initialised with
         # default parameters!
         # high- and lo_agent need to be explicitly set
 
@@ -21,46 +29,43 @@ class MetaAgent(BaseAgent):
         # high level agent's actions will be states, i.e. goals for the LL agent
         self.hi_agent = hi_agent(
             state_space=state_space, action_space=state_space)
-        
+
         # low level agent's states will be (state, goal) concatenated
         self.lo_agent = lo_agent(
-            state_space=self.concat_boxes(state_space, state_space), action_space=action_space)
-        
+            state_space=(2, *state_space.shape), action_space=action_space)
+
         self.c = c  # number of time steps between high level actions
-        self.t = 0  # step counter
+        self.t = 0  # step counter (resets after every c steps)
 
-        self.hi_rewards = 0 # collects rewads for HL agent, applied every c steps
+        self.hi_rewards = 0  # collects rewads for HL agent, applied every c steps
 
-        self.hi_state = None # state in which HL agent last took an action
+        self.hi_state = None  # state in which HL agent last took an action
+
+        # these will record sequences necessary for off-policy relabelling later
+        self.lo_action_seq = np.empty((c, *action_space.shape))
+        self.lo_state_seq = np.empty((c, *state_space.shape))
 
         # we won't need networks etc here
 
     @staticmethod
-    def concat_boxes(box1, box2):
-        """joins two gym Box objects (observation spaces) into one"""
-        return gym.spaces.Box(
-            low=np.concatenate([box1.low, box2.low]),
-            high=np.concatenate([box1.high, box2.high]))
-
-    @staticmethod
     def intrinsic_reward(state, goal, action, next_state):
-        # as defined in HIRO paper, eqn (3)
-        # difference: here we define goal as a state, not an increment
-        # action does not figure in the formula - this is apparently deliberate
-        # todo - make this a customisable function?
+        """
+        a reward function for the LoAgent as defined in HIRO paper, eqn (3)
 
+        difference: here we define goal as a state, not an increment (todo? does it matter?)
+        note: action does not figure in the formula - this is apparently deliberate
+        todo - make this a customisable function?
+        """
         return -1 * np.linalg.norm(goal - next_state)
 
-    def act(self, state):
+    def act(self, state, training=False):
+
         # is it time for a high-level action?
         if self.t % self.c == 0:
+            self.t = 0
 
             # HL agent picks a new state from space and sets it as LL's goal
-            self.goal = self.hi_agent.act(state)
-            
-            # these will record sequences necessary for off-policy relabelling later
-            self.lo_action_seq = []
-            self.lo_state_seq = []
+            self.goal = self.hi_agent.act(state, training)
 
             # save for later training
             self.hi_state = state
@@ -68,32 +73,49 @@ class MetaAgent(BaseAgent):
             # since our goal is a state rather than an increment, a goal transition function h() should not be needed, right?
 
         # action in environment comes from low level agent
-        lo_action = self.lo_agent.act(np.concatenate(state, self.goal))
+        lo_action = self.lo_agent.act(np.stack([state, self.goal]), training)
 
-        self.lo_state_seq.append(state)
-        self.lo_action_seq.append(lo_action)
+        self.lo_state_seq[self.t] = state
+        self.lo_action_seq[self.t] = lo_action
+
+        self.t += 1
 
         return lo_action
 
     def train(self, state, action, reward: float, next_state, done: bool):
-        
+
         # accumulate rewards for HL agent
         self.hi_rewards += reward
 
-        # provide LL agent with intrinisic reward
+        # provide LL agent with intrinsic reward
         lo_reward = self.intrinsic_reward(state, self.goal, action, next_state)
 
-        # The lower-level policy will store the experience 
-        # (st, gt, at, rt, st+1, h(st, gt, st+1)) 
+        # The lower-level policy will store the experience
+        # (st, gt, at, rt, st+1, h(st, gt, st+1))
         # for off-policy training.
         # note: if the hi-agent picks a new goal in the next step, then this line will not be quite right.
         # but maybe it's actually better this way...
 
-        self.lo_agent.train(np.concatenate(state, self.goal), action, lo_reward, np.concatenate(next_state, self.goal), done, relabel=False)
-
-        # TODO: create lo_state_seq, lo_action_seq
+        self.lo_agent.train(
+            np.stack([state, self.goal]),
+            action,
+            lo_reward,
+            np.stack([next_state, self.goal]),
+            done,
+            relabel=False)
 
         # is it time to train the HL agent?
         if self.t % self.c == 0:
-            self.hi_agent.train(self.hi_state, self.goal, self.hi_rewards, next_state, done, relabel=True, lo_state_seq=lo_state_seq, lo_action_seq=lo_action_seq, lo_current_policy=lo_agent.act)
+            self.hi_agent.train(
+                self.hi_state,
+                self.goal,
+                self.hi_rewards,
+                next_state,
+                done,
+                relabel=True,
+                lo_state_seq=self.lo_state_seq,
+                lo_action_seq=self.lo_action_seq,
+                lo_current_policy=self.lo_agent.act)
+
+            # reset this
             self.hi_rewards = 0
